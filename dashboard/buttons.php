94de15156caab8f9434d4f12ff01615d4e3fce65
Maximiliano Caro <caromaximiliano53@gmail.com>
2022-10-14 01:27:00 -0300

    Version-Crud-PHP

---
+++ dashboard/buttons.php
@@ -0,0 +1,135 @@
+<?php require_once "vistas/parte_superior.php"?>
+
+<!--INICIO del cont principal-->
+<div class="container">
+    <h1>Especialistas</h1>
+    
+    
+    
+ <?php
+include_once 'bd/conexion.php';
+$objeto = new Conexion();
+$conexion = $objeto->Conectar();
+
+$consulta = "SELECT user_id, username, first_name, last_name, gender, password, status, FROM especialistas";
+$resultado = $conexion->prepare($consulta);
+$resultado->execute();
+$data=$resultado->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+
+<div class="container">
+        <div class="row">
+            <div class="col-lg-12">            
+            <button id="btnNuevo" type="button" class="btn btn-success" data-toggle="modal">Nuevo</button>    
+            </div>      
+            </div>    
+        </div>    
+    </div>    
+    <br>  
+
+    <div class="container caja text-white">
+        <div class="row">
+            <div class="col-lg-12">
+            <div class="table-responsive bg-dark">        
+                <table id="tablaUsuarios" class="table table-striped table-bordered table-condensed  text-white" style="width:100%" >
+                    <thead class="text-center bg-info">
+                        <tr>
+                            <th>User_Id</th>
+                            <th>Especialistas</th>
+                            <th>Nombre</th>                                
+                            <th>Apellido</th>  
+                            <th>Genero</th>
+                            <th>Password</th>
+                            <th>Status</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                            <?php                            
+                            foreach($data as $dat) {                                                        
+                            ?>
+                            <tr>
+                                <td><?php echo $dat['user_id'] ?></td>
+                                <td><?php echo $dat['username'] ?></td>
+                                <td><?php echo $dat['first_name'] ?></td>
+                                <td><?php echo $dat['last_name'] ?></td>
+                                <td><?php echo $dat['gender'] ?></td> 
+                                <td><?php echo $dat['password'] ?></td> 
+                                <td><?php echo $dat['status'] ?></td> 
+                                <td></td>
+                            </tr>
+                            <?php
+                                }
+                            ?>                                
+                        </tbody>                
+                </table>               
+            </div>
+            </div>
+        </div>  
+    </div>   
+
+<!--Modal para CRUD-->
+<div class="modal fade" id="modalCRUD" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        <form id="formUsuarios">    
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-lg-6">
+                    <div class="form-group">
+                    <label for="" class="col-form-label"><strong>Especialista</strong></label>
+                    <input type="text" class="form-control" id="username">
+                    </div>
+                    </div>
+                    <div class="col-lg-6">
+                    <div class="form-group">
+                    <label for="" class="col-form-label"><strong>Nombre</strong></label>
+                    <input type="text" class="form-control" id="first_name">
+                    </div> 
+                    </div>    
+                </div>
+                <div class="row"> 
+                    <div class="col-lg-6">
+                    <div class="form-group">
+                    <label for="" class="col-form-label"><strong>Apellido</strong></label>
+                    <input type="text" class="form-control" id="last_name">
+                    </div>               
+                    </div>
+                    <div class="col-lg-6">
+                    <div class="form-group">
+                    <label for="" class="col-form-label"><strong>Genero</strong></label>
+                    <input type="text" class="form-control" id="gender">
+                    </div>
+                    </div>  
+                </div>
+                <div class="row">
+                    <div class="col-lg-9">
+                        <div class="form-group">
+                        <label for="" class="col-form-label"><strong>Password</strong></label>
+                        <input type="text" class="form-control" id="password">
+                        </div>
+                    </div>    
+                    <div class="col-lg-3">    
+                        <div class="form-group">
+                        <label for="" class="col-form-label"><strong>Status</strong></label>
+                        <input type="number" class="form-control" id="status">
+                        </div>            
+                    </div>    
+                </div>                
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-light" data-dismiss="modal">Cancelar</button>
+                <button type="submit" id="btnGuardar" class="btn btn-dark">Guardar</button>
+            </div>
+        </form>    
+        </div>
+    </div>
+</div>  
+<!--FIN del cont principal-->
+<?php require_once "vistas/vista_especia.php"?>
---
+++ dashboard/vistas/vista_emple.php
@@ -0,0 +1,74 @@
+</div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-dark">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span style="color:white ;">Copyright &copy; |2022|Maximiliano-Caro</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+  <!-- Logout Modal-->
+  <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">¿Confirma salir y cerrar Sesión?</h5>
+          <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">×</span>
+          </button>
+        </div>
+        <div class="modal-body"></div>
+        <div class="modal-footer">
+          <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancelar</button>
+          <a class="btn btn-primary" href="../bd/logout.php">Salir</a>
+  
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="js/sb-admin-2.min.js"></script>
+
+ <!-- jQuery, Popper.js, Bootstrap JS -->
+ <script src="assets/jquery/jquery-3.3.1.min.js"></script>
+    <script src="assets/popper/popper.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+      
+    <!-- datatables JS -->
+    <script type="text/javascript" src="assets/datatables/datatables.min.js"></script>   
+
+  
+    <!-- datatables JS -->
+    <script type="text/javascript" src="vendor/datatables/datatables.min.js"></script>    
+    <!-- código propio JS --> 
+    
+    
+    <script type="text/javascript" src="emple.js"></script>
+    
+
+</body>
+
+</html>
